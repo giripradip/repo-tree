5b963bcb11e751c8eb160cac7ef0672e68a49349
Pradip Giri <mepradipjee@gmail.com>
2019-11-19 20:30:19 +0100

    Clear unwanted stuff from about page and add messages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,102 +1,13 @@
 <template>
-  <v-card>
-    <v-card-title class="indigo white--text headline">
-      User Directory
-    </v-card-title>
-    <v-row
-      class="pa-4"
-      justify="space-between"
-    >
-      <v-col cols="5">
-        <v-treeview
-          :active.sync="active"
-          :items="items"
-          :load-children="fetchUsers"
-          :open.sync="open"
-          activatable
-          color="warning"
-          open-on-click
-          transition
-        >
-          <template v-slot:prepend="{ item, active }">
-            <v-icon v-if="!item.children">mdi-account</v-icon>
-          </template>
-        </v-treeview>
-      </v-col>
-
-      <v-divider vertical></v-divider>
-
-      <v-col
-        class="d-flex text-center"
-      >
-        <v-scroll-y-transition mode="out-in">
-          <div
-            v-if="!selected"
-            class="title grey--text text--lighten-1 font-weight-light"
-            style="align-self: center;"
-          >
-            Select a User
-          </div>
-          <v-card
-            v-else
-            class="pt-6 mx-auto"
-            flat
-            max-width="400"
-          >
-            <v-card-text>
-
-            </v-card-text>
-
-          </v-card>
-        </v-scroll-y-transition>
-      </v-col>
+  <v-card class="about-container">
+    <v-row class="pa-4 title">
+      This application is developed for demonstration purpose only.
     </v-row>
   </v-card>
 </template>
 
-<script>
-
-const pause = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-export default {
-  data: () => ({
-    active: [],
-    avatar: null,
-    open: [],
-    users: [],
-  }),
-
-  computed: {
-    items() {
-      return [
-        {
-          name: 'Users',
-          children: this.users,
-        },
-      ];
-    },
-    selected() {
-      if (!this.active.length) return undefined;
-
-      const id = this.active[0];
-
-      return this.users.find(user => user.id === id);
-    },
-  },
-
-  methods: {
-    async fetchUsers(item) {
-      // Remove in 6 months and say
-      // you've made optimizations! :)
-
-      console.log(item);
-      await pause(1500);
-
-      return fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(json => (item.children.push(...json)))
-        .catch(err => console.warn(err));
-    },
-  },
-};
-</script>
+<style scoped>
+.about-container {
+  margin: 25px;
+}
+</style>
