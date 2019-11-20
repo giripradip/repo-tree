084a11a71bbdf90416e548ccdae2ef76f281588e
Pradip Giri <mepradipjee@gmail.com>
2019-11-20 22:36:57 +0100

    Add item route

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
+      <div class="menu-item-container">
+        <v-btn text exact-active-class to="/">Home</v-btn>
+        <v-btn text exact-active-class to="/preload">Preload</v-btn>
+        <v-btn text exact-active-class to="/about">About</v-btn>
+      </div>
     </v-app-bar>
 
     <v-content>
-     <router-view/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
   data: () => ({
     //
   }),
 };
 </script>
+
+<style scoped >
+.menu-item-container {
+  display: block;
+  margin: 0 auto;
+}
+</style>
