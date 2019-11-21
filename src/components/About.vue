12895a3b80ae598555a829696932e97fe00ed22f
Pradip Giri <mepradipjee@gmail.com>
2019-11-21 21:58:31 +0100

    Add help text

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,6 +2,9 @@
   <v-card class="about-container">
     <v-row class="pa-4 title">
       This application is developed for demonstration purpose only.
+      In the Home page repo information is preloaded and displayed one
+      step ahead when item is clicked whereas in the Simple
+      load page the child information is loaded when the parent is clicked.
     </v-row>
   </v-card>
 </template>
